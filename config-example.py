a3859016a26ca4340451c4d948025e9cb2739862
Sai Raj <39055732+sairajzero@users.noreply.github.com>
2024-07-18 05:32:10 -0400

    merge config files for api and scanner

---
+++ config-example.py
@@ -0,0 +1,30 @@
+# general configs
+DATA_PATH = "/home/production/Dev/flo-token-tracker"
+APP_ADMIN = "oWooGLbBELNnwq8Z5YmjoVjw8GhBGH3qSP"
+
+# API configs
+apiUrl = 'https://blockbook.ranchimall.net/api'
+FLO_DATA_DIR = "/home/production/.flo"
+HOST = "localhost"
+PORT = 8080
+debug_status = False
+sse_pubKey = '<public key in the format of pybtc python library>'
+
+
+# your apilayer.net access key
+apilayerAccesskey = '<accesskey>'
+
+NET = "mainnet"
+
+START_BLOCK = 3387900
+BLOCKBOOK_NETURL = "https://blockbook.ranchimall.net/"
+TOKENAPI_SSE_URL = "https://ranchimallflo.duckdns.org/"
+MAINNET_BLOCKBOOK_SERVER_LIST = ["https://blockbook.ranchimall.net/"]
+TESTNET_BLOCKBOOK_SERVER_LIST = ["https://blockbook-testnet.ranchimall.net/"]
+IGNORE_BLOCK_LIST = 902446
+#IGNORE_TRANSACTION_LIST = "b4ac4ddb51188b28b39bcb3aa31357d5bfe562c21e8aaf8dde0ec560fc893174"
+
+"""?NOT USED?
+FLO_CLI_PATH = "/usr/local/bin/flo-cli"
+
+"""
--- src/api/config-example.py
+++
@@ -1,8 +0,0 @@
-dbfolder = ''
-debug_status = False
-sse_pubKey = '<public key in the format of pybtc python library>'
-apiUrl = 'https://flosight.duckdns.org/api/'
-
-# your apilayer.net access key
-apilayerAccesskey = '<accesskey>'
-
